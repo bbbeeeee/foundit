94d06574c1d925ecd660c522a39c4037bf0f6612
Brandon Truong <me@brandontruong.com>
2014-08-09 13:18:45 -0400

    First commit with template

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'foundit'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/foundit-development'
+    
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'foundit'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/foundit-test'
+    
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'foundit'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/foundit-production'
+    
+  }
+};
+
+module.exports = config[env];
