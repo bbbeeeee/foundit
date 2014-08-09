dabda2be8838415c3461063879a175cf3da2657c
Brandon Truong <me@brandontruong.com>
2014-08-09 13:48:22 -0400

    changed directories

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ db.on('error', function () {
   throw new Error('unable to connect to database at ' + config.db);
 });
 
-var modelsPath = '../app/models';
+var modelsPath = __dirname + '/app/models';
 fs.readdirSync(modelsPath).forEach(function (file) {
   if (file.indexOf('.js') >= 0) {
     require(modelsPath + '/' + file);
@@ -17,7 +17,7 @@ fs.readdirSync(modelsPath).forEach(function (file) {
 });
 var app = express();
 
-require('../config/express')(app, config);
+require('./config/express')(app, config);
 
 require('./routes')(app);
 
