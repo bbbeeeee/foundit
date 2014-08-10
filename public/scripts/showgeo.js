46bc376850e76d99322551ed1653995d4fe8dd0a
Brandon Truong <me@brandontruong.com>
2014-08-10 04:05:10 -0400

    The ride never ends.

--- public/scripts/showgeo.js
+++ public/scripts/showgeo.js
@@ -2,7 +2,7 @@ var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 8,
+    zoom: 14,
     center: new google.maps.LatLng(42.35, -71.055)
   };
   map = new google.maps.Map(document.getElementById('map'),
