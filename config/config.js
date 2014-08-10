89a0575eb1eac9d3a6a97dc8015c52f9643bbf7a
Brandon Truong <me@brandontruong.com>
2014-08-10 07:31:33 -0400

    one more try

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ var config = {
       name: 'foundit'
     },
     port: 3000,
-    db: 'mongodb://localhost/foundit-development'
+    db: process.env.MONGOLAB_URI || 'mongodb://localhost/foundit-development'
     
   },
 
@@ -19,7 +19,7 @@ var config = {
       name: 'foundit'
     },
     port: 3000,
-    db: 'mongodb://localhost/foundit-test'
+    db: process.env.MONGOLAB_URI || 'mongodb://localhost/foundit-test'
     
   },
 
